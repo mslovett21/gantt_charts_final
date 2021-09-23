d3aff7ca70db50ac831b9fc879e178d6ff33c8df
ShubhamNagarkar <shubham.nagarkar10@gmail.com>
2021-09-22 21:04:13 -0700

    reconstruction_update

--- autoencoder.py
+++ autoencoder.py
@@ -3,12 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import warnings
 import argparse
+import glob
+import cv2
+from PIL import Image
 import os, gc, time
+import seaborn as sns
 import pickle as pkl
 warnings.filterwarnings('ignore')
 pd.options.display.float_format = '{:.0f}'.format
 
 import torch
+import torchvision
 from torch import nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms, models
@@ -30,7 +35,8 @@ import mlflow
 # add resume training
 # add more mlflow
 
-
+BATCH_SIZE = 1
+RECONSTRUCTION = True
 gc.collect()
 torch.cuda.empty_cache()
 torch.manual_seed(0)
@@ -42,9 +48,9 @@ print(DEVICE)
 def get_arguments():
 
     parser = argparse.ArgumentParser('Train or Eval AutoEncoder')
-    parser.add_argument('data_dir', type= str, help="Path to the folder with the data")
-    parser.add_argument('--epochs', type=int, default= 3, help="Number of training epochs")
-    parser.add_argument('--batch_size', type=int, default=8, help="Number of training epochs")
+    parser.add_argument('--data_dir', type= str, help="Path to the folder with the data")
+    parser.add_argument('--epochs', type=int, default= 100, help="Number of training epochs")
+    parser.add_argument('--batch_size', type=int, default=64, help="Number of training epochs")
     parser.add_argument('--img_res', type=int, default=224, help="Image resolution")
     parser.add_argument('-e','--evalmode', help ='if set looks for checkpoint and evaluate data on it', action= "store_true")
     args = parser.parse_args()
@@ -74,6 +80,7 @@ def draw_training_curves(train_losses, test_losses,curve_name, epochs,path):
 def read_res_img(res, data_dir):  
     train_transforms = transforms.Compose([transforms.Resize([res,res]),transforms.ToTensor()])
     gantt_dataset    = datasets.ImageFolder(data_dir, transform=train_transforms)
+
     return gantt_dataset
 
 
@@ -140,7 +147,7 @@ def train_model(ae_model, gantt_data_loader, gantt_dataset_test, pad, epochs, lr
         rec_loss_all.append(rec_loss)
 
         print("Epoch {}: Reconstruction Loss: {}".format(train_it, rec_loss))
-        if ep % 10 == 0:
+        if (ep+1) % 10 == 0:
             torch.save(ae_model.state_dict(), checkpoint_dir + "/ae_model_"+ str(train_it) + ".pth")
         
         img = vis_reconstruction(ae_model,gantt_dataset_test, str(train_it), pad, recon_imgs_dir)
@@ -162,7 +169,23 @@ def train_model(ae_model, gantt_data_loader, gantt_dataset_test, pad, epochs, lr
     return rec_loss_all, test_loss_all
 
 
+def get_reconstruction_error(test_loader, model, pad):
 
+    criterion = nn.MSELoss().to(DEVICE)
+    model.to(DEVICE)
+    model.eval()
+    losses = []
+
+    for image, label in test_loader:
+
+        image = image.to(DEVICE)
+        dec = model(image)
+
+        result = F.pad(input= dec, pad=(0, pad, pad, 0), mode='constant', value=0)
+        loss = criterion(result,image)
+        losses.append(loss.item())
+
+    return losses
 
 def eval_model(ae_model,gantt_data_loader,gantt_dataset,dataset_name,pad,recon_imgs_dir):
    
@@ -210,6 +233,63 @@ def get_AE_params(img_res):
 
 
 
+class Dataset_Loader(torch.utils.data.Dataset):
+    
+    def __init__(self, data, label,  transform = None):
+        self.data = data
+        self.label = label
+        self.transform = transform
+        self.length = len(self.data)
+
+    def __len__(self):
+        return self.length
+  
+    def __getitem__(self, idx):
+        
+        img = cv2.imread(self.data[idx])
+        image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if self.transform:
+            image =  self.transform(image)
+        label = self.label[idx]
+
+        return image, label
+
+
+def get_anomalies(res):
+
+    normal_path = '/home/shubham/gantt_charts_final/datasets/gantt_chart_images_224by224_nogenome_NORMAL/test/normal/'
+    cpu_path = '/home/shubham/gantt_charts_final/datasets/gantt_chart_images_224by224_nogenome_CPU/test/cpu/'
+    hdd_path = '/home/shubham/gantt_charts_final/datasets/gantt_chart_images_224by224_nogenome_HDD/test/hdd/'
+    loss_path = '/home/shubham/gantt_charts_final/datasets/gantt_chart_images_224by224_nogenome_LOSS/test/loss/'
+
+    anomalies = glob.glob(cpu_path + '*.png')
+    anomalies += glob.glob(hdd_path + '*.png')
+    anomalies += glob.glob(loss_path + '*.png')
+    anomaly_labels = [1]*len(anomalies)
+
+    print("Size of anomalies: ",len(anomalies))
+
+    normal = glob.glob(normal_path + '*.png')
+    normal_labels = [0]*len(normal)
+
+    print("Size of normal data: ",len(normal))    
+    
+    test_transforms = transforms.Compose([transforms.ToPILImage(), transforms.Resize([res,res]),transforms.ToTensor()])
+
+    anomaly_dataset = Dataset_Loader(anomalies, anomaly_labels, test_transforms)
+    anomaly_data_loader = DataLoader(anomaly_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+
+    normal_dataset = Dataset_Loader(normal, normal_labels, test_transforms)
+    normal_data_loader = DataLoader(normal_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+
+    return anomaly_data_loader, normal_data_loader
+
+def plot_distplot(error, figname):
+
+    sns.displot(error, bins=50, kde=True)
+    plt.savefig(figname)
+
+
 def main():
 
     time_start = time.time()
@@ -224,7 +304,7 @@ def main():
     data_dir   = args.data_dir
     batch_size = args.batch_size
     img_resolution = args.img_res
-    lr             = 0.0001
+    lr             = 0.001
     checkpoint_number = 600
 
     mlflow.log_param("data_dir", data_dir)
@@ -249,13 +329,26 @@ def main():
     nworkers   = 4 # number of wrokers used for efficient data loading
     ae_model   = AutoEncoder(deep_layer, shallow_layer, z_emb_size, dim, features, var_stride)
 
-    if args.evalmode == True:
-        for dataset_dir, dataset_name in zip([test_data_dir, train_data_dir], ["test", "train"]):
+    args.evalmode = True # DID THIS TEMPORARILY TO TEST. REMOVE IT ONCE YOU FIGURE OUT --EVAL
 
-            gantt_dataset     = read_res_img(img_resolution, dataset_dir)
-            gantt_data_loader = DataLoader(gantt_dataset, batch_size=batch_size, shuffle=True, num_workers=nworkers)
-            ae_model.load_state_dict(torch.load(checkpoint_path))
-            eval_model(ae_model, gantt_data_loader,gantt_dataset,dataset_name,pad,recon_imgs_dir)
+    if args.evalmode == True:
+        if RECONSTRUCTION:
+            anomaly_data_loader, normal_data_loader = get_anomalies(img_resolution)
+            load_ckp = rel_path + "/checkpoints/checkpoint_" + dir_string + "_20210920-144235/ae_model_90.pth" 
+            ae_model.load_state_dict(torch.load(load_ckp))
+            anomaly_error = get_reconstruction_error(anomaly_data_loader, ae_model, pad)
+            normal_error = get_reconstruction_error(normal_data_loader, ae_model, pad)
+            plot_distplot(anomaly_error, 'anomaly_reconstruction.png')
+            plot_distplot(normal_error, 'normal_reconstruction.png')
+            
+        else:
+
+            for dataset_dir, dataset_name in zip([test_data_dir, train_data_dir], ["test", "train"]):
+
+                gantt_dataset     = read_res_img(img_resolution, dataset_dir)
+                gantt_data_loader = DataLoader(gantt_dataset, batch_size=batch_size, shuffle=True, num_workers=nworkers)
+                ae_model.load_state_dict(torch.load(checkpoint_path))
+                eval_model(ae_model, gantt_data_loader,gantt_dataset,dataset_name,pad,recon_imgs_dir)
     else:
         gantt_dataset_train = read_res_img(img_resolution, train_data_dir)
         gantt_dataset_test  = read_res_img(img_resolution, test_data_dir)
